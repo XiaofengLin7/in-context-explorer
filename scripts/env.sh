91f0303a687c98e1a8457dde356a8f3af7316725
XiaofengLin7 <xfl199801@gmail.com>
2025-10-20 21:42:49 -0400

    Initial commit

---
+++ scripts/env.sh
@@ -0,0 +1,5 @@
+ALFWORLD_DATA=/projectnb/replearn/xfl/alfworld/data_storage
+N_GPUS=4
+
+QWEN25_1dot5B_INSTRUCT=/replearn/xfl/models/models--Qwen--Qwen2.5-1.5B-Instruct/snapshots/989aa7980e4cf806f80c7fef2b1adb7bc71aa306
+QWEN3_1dot7B=/replearn/xfl/models--Qwen--Qwen3-1.7B/snapshots/70d244cc86ccca08cf5af4e1e306ecf908b1ad5e
