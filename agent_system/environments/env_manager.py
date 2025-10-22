096e4df2ab079005807d036cff50b4a49e902099
Yilei Chen <ylchen9@scc-210.scc.bu.edu>
2025-10-22 15:37:34 -0400

    working

--- agent_system/environments/env_manager.py
+++ agent_system/environments/env_manager.py
@@ -42,6 +42,21 @@ def set_gamefile(infos, gamefile):
     return infos
 
 
+class WebVoyagerManager(EnvironmentManagerBase):
+    """
+    Working
+    """
+    def __init__(self, envs, projection_f, config):
+        pass
+
+    def reset(self):
+        driver_task = webdriver.Chrome(options=options)
+        driver_task = set_window_size()
+        pass
+
+    def step(self):
+        pass
+
 class SearchEnvironmentManager(EnvironmentManagerBase):
     """
     EnvironmentManager for SearchEnv.
@@ -703,6 +718,16 @@ def make_envs(config):
         projection_f = partial(appworld_projection)
         envs = AppWorldEnvironmentManager(_envs, projection_f, config)
         val_envs = AppWorldEnvironmentManager(_val_envs, projection_f, config)
+        return envs, val_envs
+    elif "webvoyager" in config.env.env_name.lower():
+        from agent_system.environments.env_package.webvoyager import build_webvoyager_envs, webvoyager_projection
+        env_kwargs = {}
+        _envs = build_webvoyager_envs(config.env.seed, config.data.train_batch_size, group_n, is_train=True, env_kwargs=env_kwargs, resources_per_worker=resources_per_worker)
+        _val_envs = build_sokoban_envs(config.env.seed + 1000, config.data.val_batch_size, 1, is_train=False, env_kwargs=env_kwargs, resources_per_worker=resources_per_worker)
+        
+        projection_f = partial(webvoyager_projection)
+        envs = WebVoyagerEnvironmentManager(_envs, projection_f, config)
+        val_envs = WebVoyagerEnvironmentManager(_val_envs, projection_f, config)
         return envs, val_envs
     else:
         print("Environment not supported")
---
+++ agent_system/environments/env_package/webvoyager/projection.py
@@ -0,0 +1,13 @@
+from typing import List
+import re
+
+# TODO
+def webvoyager_projection(actions: List[str]):
+    """
+    A function to process the actions.
+    actions: the list of actions to be processed, it is a list of strings.
+    Expected format:
+        <think>some reasoning...</think><action>up/down/left/right/still</action>
+    """
+    
+    pass
