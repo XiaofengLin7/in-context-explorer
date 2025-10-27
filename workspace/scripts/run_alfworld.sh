51232b89a7b9ad3dd77b590a47a5d1563a1c575c
srzhu97 <srzhu97@gmail.com>
2025-10-27 06:15:20 +0000

    set up initial workflow

---
+++ workspace/scripts/run_alfworld.sh
@@ -0,0 +1,73 @@
+set -x
+source /home/jobuser/resources/scripts/mlflow/setup_mlflow_hf.sh
+MODEL_PATH=/shared/public/elr-models/Qwen/Qwen2.5-1.5B-Instruct/2fd50615a2a9792d223eba8e0741aa90ef21a869
+
+export ALFWORLD_DATA=/shared/public/sharing/hsang/alfworld_data
+N_GPUS=4
+ENGINE=${1:-vllm}
+export VLLM_ATTENTION_BACKEND=FLASH_ATTN
+project_name=${MLFLOW_EXPERIMENT_NAME}
+num_cpus_per_env_worker=0.1 # The CPU resource allocated for each environment worker. If you want to use less CPU resources, you can decrease this value.
+
+train_data_size=16
+val_data_size=128
+group_size=8
+success_coef=50
+prompt_type=summary
+experiment_name=grpo_qwen2.5_1.5b_success_coef_${success_coef}_prompt_type_${prompt_type}
+# We only use data preparation to indicate the modality and the data size.
+cd /home/jobuser/resources/in-context-explorer
+python -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files=/shared/public/sharing/hsang/verl-agent_text/train.parquet \
+    data.val_files=/shared/public/sharing/hsang/verl-agent_text/test.parquet \
+    data.train_batch_size=$train_data_size \
+    data.val_batch_size=$val_data_size \
+    data.max_prompt_length=2048 \
+    data.max_response_length=512 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    data.return_raw_chat=True \
+    actor_rollout_ref.model.path=$MODEL_PATH \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.01 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.name=$ENGINE \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.enable_chunked_prefill=False \
+    actor_rollout_ref.rollout.enforce_eager=False \
+    actor_rollout_ref.rollout.free_cache_engine=False \
+    actor_rollout_ref.rollout.val_kwargs.temperature=0.4 \
+    actor_rollout_ref.rollout.val_kwargs.do_sample=True \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.use_invalid_action_penalty=True \
+    actor_rollout_ref.actor.invalid_action_penalty_coef=0.1 \
+    algorithm.use_kl_in_reward=False \
+    env.env_name=alfworld/AlfredTWEnv \
+    env.seed=0 \
+    env.prompt_type=$prompt_type \
+    env.success_coef=$success_coef \
+    env.max_steps=50 \
+    env.rollout.n=$group_size \
+    env.history_length=4 \
+    env.resources_per_worker.num_cpus=$num_cpus_per_env_worker \
+    trainer.critic_warmup=0 \
+    trainer.logger=['console','mlflow'] \
+    trainer.project_name=$project_name  \
+    trainer.experiment_name=$experiment_name-${FLYTE_INTERNAL_EXECUTION_ID} \
+    trainer.n_gpus_per_node=$N_GPUS \
+    trainer.nnodes=1 \
+    trainer.save_freq=300 \
+    trainer.test_freq=5 \
+    trainer.total_epochs=500 \
+    trainer.val_before_train=True $@
