533142da82f235fb06b649e3707a89550de3e1ec
XiaofengLin7 <xfl199801@gmail.com>
2025-10-29 22:27:04 -0400

    webshop scripts with success coef and prompt type

--- examples/grpo_trainer/run_webshop.sh
+++ examples/grpo_trainer/run_webshop.sh
@@ -8,6 +8,10 @@ train_data_size=16
 val_data_size=128
 group_size=8
 N_GPUS=4
+success_coef=0
+prompt_type=summary
+history_length=15
+experiment_name=grpo_qwen2.5_1.5b_success_coef_${success_coef}_prompt_type_${prompt_type}_history_length_${history_length}
 # We only use data preparation to indicate the modality and the data size.
 python3 -m examples.data_preprocess.prepare \
     --mode 'text' \
@@ -53,15 +57,18 @@ python3 -m verl.trainer.main_ppo \
     env.env_name=Webshop \
     env.seed=0 \
     env.max_steps=15 \
+    env.success_coef=${success_coef} \
+    env.prompt_type=${prompt_type} \
+    env.history_length=${history_length} \
     env.rollout.n=$group_size \
     env.resources_per_worker.num_cpus=$num_cpus_per_env_worker \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='verl_agent_webshop' \
-    trainer.experiment_name='grpo_qwen2.5_1.5b' \
+    trainer.experiment_name=${experiment_name} \
     trainer.n_gpus_per_node=$N_GPUS \
     trainer.nnodes=1 \
-    trainer.save_freq=200 \
+    trainer.save_freq=300 \
     trainer.test_freq=5 \
-    trainer.total_epochs=200 \
+    trainer.total_epochs=300 \
     trainer.val_before_train=False $@
