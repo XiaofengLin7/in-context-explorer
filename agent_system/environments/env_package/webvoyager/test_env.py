a9f94f1bc77fe731e789a8808b3999442f1ec460
Yilei Chen <ylchen9@scc-210.scc.bu.edu>
2025-11-03 13:33:19 -0500

    add webarena test

--- agent_system/environments/env_package/webvoyager/test_env.py
+++ agent_system/environments/env_package/webvoyager/test_env.py
@@ -1,29 +1,39 @@
 # Test if env works
 
-test_gym_env = False
+test_gym_env = True
 
 if test_gym_env:
-    from webgym import WebVoyagerEnv
+    from agent_system.environments.env_package.webvoyager.webgym import WebVoyagerEnv
+    import json
+    from omegaconf import OmegaConf
     
     env = WebVoyagerEnv(
             api_key="your-api-key-here",
             headless=True,
             text_only=False,
+            env_config=OmegaConf.load("agent_system/environments/env_package/webvoyager/configs/configs.yaml")
         )
-        
+
     # Example task
-    task = {"web_name": "Cambridge Dictionary", "id": "Cambridge Dictionary--29", "ques": "Go to the Plus section of Cambridge Dictionary, find Image quizzes and do an easy quiz about Animals and tell me your final score.", "web": "https://dictionary.cambridge.org/"}
+    #task = {"web_name": "Cambridge Dictionary", "id": "Cambridge Dictionary--29", "ques": "Go to the Plus section of Cambridge Dictionary, find Image quizzes and do an easy quiz about Animals and tell me your final score.", "web": "https://dictionary.cambridge.org/"}
+    data_file = "/projectnb/noc-lab/ylchen/ICX/tasks/webarena_test_data.jsonl"
+    tasks = []
+    with open(data_file, 'r', encoding='utf-8') as f:
+        for line in f:
+            tasks.append(json.loads(line))
+    
+    task = tasks[0]
 
     # Reset environment
     obs, info = env.reset(task)
-    #print(f"Reset obs: {obs['text']}")
+    print(f"Reset obs: {obs}")
 
     # Example actions
     actions = [
-        {'action_key': 'type', 'element': 15, 'content': 'test'},
         {'action_key': 'scroll', 'element': -1, 'content': 'down'},
         {'action_key': 'wait'},
         {'action_key': 'click', 'element': 1},
+        {'action_key': 'type', 'element': 15, 'content': 'test'},
         {'action_key': 'answer', 'answer_content': 'Finish the task'}
     ]
 
@@ -31,7 +41,7 @@ if test_gym_env:
     for i, action in enumerate(actions):
         obs, reward, done, info = env.step(action)
         print(f"Step {i+1}: Action {action['action_key']}, Reward {reward}, Done {done}")
-        
+        print(f"obs at step {i+1}: {obs}")
         if done:
             print(f"fail obs: {env.fail_obs}")
             break
@@ -40,7 +50,7 @@ if test_gym_env:
     env.close()
 else:
     import os
-    from envs import WebVoyagerMultiProcessEnv
+    from agent_system.environments.env_package.webvoyager.envs import WebVoyagerMultiProcessEnv
     from pprint import pprint
 
     base_dir = os.path.dirname(__file__)
